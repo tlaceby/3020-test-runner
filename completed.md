a9d7133c1e5853d42ccb69c1ec62469876812674
tlaceby <tylerbadgertutorials@gmail.com>
2024-01-26 13:28:09 -0700

    gen

---
+++ completed.md
@@ -0,0 +1,5 @@
+# 3020 Excersises List 
+Generated On: Fri Jan 26 2024 13:27:02
+Completed 0 of 1 excersises.
+
+- [ ] .git
--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 
+const dir = Deno.args[0];
+
+if (!dir) {
+    console.error("Invalid cli usage. Must supply a path to place the generated md file.")
+}
+
+console.log(dir);
 let md = `# 3020 Excersises List 
 Generated On: ${new Date().toDateString()} ${new Date().toTimeString().split(" ")[0]}
 Completed $COMPLETED of $TOTAL excersises.\n\n`;
@@ -31,4 +38,4 @@ for await (const dirEntry of Deno.readDir('./')) {
 
 md = md.replaceAll("$COMPLETED", totalCompleted.toString());
 md = md.replaceAll("$TOTAL", count.toString());
-Deno.writeTextFileSync("completed.md", md);
+Deno.writeTextFileSync(dir + "completed.md", md);
